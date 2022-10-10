731616ef933b6c3250d76e2c8a1e9080c92d4d89
VOLODYMYR BASHTANYUK <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2022-10-10 13:20:59 +0300

    delet link

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { Wrapper, Item, LinkStyle } from './MovieItemStyle';
 
 export const MovieItem = ({ items }) => {
