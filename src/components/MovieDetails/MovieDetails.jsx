a4687639fc338649e2ddcd813bce2f2247797a11
VOLODYMYR BASHTANYUK <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2022-10-13 20:54:43 +0300

    correction with location

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -20,8 +20,7 @@ export const MovieDetails = ({ data }) => {
 
   const year = new Date(release_date);
   const location = useLocation();
-  const backLink = location.state?.from &&
-"/movies/:movieId";
+  const backLink = location.state?.from;
   const genre = genres.map(({ id, name }) => {
     return <Genre key={id}>{name}</Genre>;
   });
